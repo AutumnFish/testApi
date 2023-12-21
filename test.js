bb658a2c42602e06589aeacdac683e9dc8da0241
autumnfish <517729329@qq.com>
2023-12-21 10:22:17 +0800

    remove log

--- test.js
+++ test.js
@@ -1,13 +1,9 @@
 const path = require('path')
 const fs = require('fs')
 
-const totalArr = JSON.parse(
-  fs.readFileSync(path.join(__dirname, './data/lol_details_duowan.json'))
-)
+const totalArr = JSON.parse(fs.readFileSync(path.join(__dirname, './data/lol_details_duowan.json')))
 
-const qqArr = JSON.parse(
-  fs.readFileSync(path.join(__dirname, './data/lol_qq.json'))
-)
+const qqArr = JSON.parse(fs.readFileSync(path.join(__dirname, './data/lol_qq.json')))
 
 totalArr.forEach(v => {
   qqArr.forEach(v1 => {
@@ -18,15 +14,5 @@ totalArr.forEach(v => {
   })
 })
 
-qqArr.forEach(v => {
-  if (v.name == v.title) {
-    //  console.log(v.name+'有问题')
-  }
-})
-
-//  console.log(qqArr)
-fs.writeFileSync(
-  path.join(__dirname, './data/lol_qq.json'),
-  JSON.stringify(qqArr)
-)
+fs.writeFileSync(path.join(__dirname, './data/lol_qq.json'), JSON.stringify(qqArr))
 console.log('finish')
